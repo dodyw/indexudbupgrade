2cb570b15dc7dc863c5a1a6a544173bbabf2127e
dody <dodyrw@gmail.com>
2011-04-20 10:54:59 +0700

    upgrade from 5.4 to deluxe 2.4

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Indexu DB Upgrade</title>
+  <style>
+    html {
+      font-family: verdana;
+      font-size: 12px;
+    }
+    h1 {
+      font-size: 18px;
+    }
+    .explain {
+      margin: 20px;
+      padding: 10px;
+      border: 1px solid #000;
+      background: #ffc;
+    }
+  </style>
+</head>
+<body>
+  <h1>Indexu DB Upgrade</h1>
+  <div class="explain">
+    This script will upgrade indexu database. 
+    Before running this script, edit db.php to set the mysql database.
+    Also make sure you have sql dump backup.     
+  </div>
+  
+  <div class="option">
+    <a href="54_d24.php">Click here</a> to upgrade database from indexu 5.4 to indexu deluxe 2.4
+  </div>  
+  
+</body>
+</html>
